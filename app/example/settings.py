c05ef80aad95d5e002db2cd530ea40aff8310390
endersonpb <endersonpb0@gmail.com>
2023-05-10 15:42:29 -0300

    up

--- app/example/settings.py
+++ app/example/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'banco',
         'USER': 'root',
         'PASSWORD': '14072002',
-        'HOST': 'localhost',
+        'HOST': '172.18.0.1',
         'PORT': '3306'
     }
 }
